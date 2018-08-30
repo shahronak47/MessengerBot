c573b14d33eeca9953ddb90d92266f85e996fa61
Ronak Shah <shahronak47@yahoo.in>
2018-08-30 22:12:31 +0800

    Change routing address

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
 
 #We will receive messages that Facebook sends our bot at this endpoint
-@app.route("/webhook", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
